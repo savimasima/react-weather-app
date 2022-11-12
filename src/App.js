32880daf08677e1d69d8c1ed7332c79f5ee4103f
Savimasima <savimasima@gmail.com>
2022-11-12 13:53:24 +0000

    Added github link

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by savimasima and is{" "}
+        <a 
+          href="https://github.com/savimasima/react-weather-app" 
+          target="_blank"
+        >open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
