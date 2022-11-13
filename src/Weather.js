61391c5ac54addef5370992d97ad78d0b6592e9f
Savimasima <savimasima@gmail.com>
2022-11-13 15:40:58 +0000

    Added date on the weather

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import FormattedDate from "./FormattedDate";
 import axios from "axios";
 import "./Weather.css";
 import 'bootstrap/dist/css/bootstrap.css';
@@ -10,7 +11,7 @@ export default function Weather(props) {
         setWeatherData({
             ready: true,
             temperature: response.data.main.temp,
-            date: "Monday 07:00",
+            date: new Date(response.data.dt * 1000),
             humidity: response.data.main.humidity,
             wind: response.data.wind.speed,
             city: response.data.name,
@@ -43,7 +44,9 @@ export default function Weather(props) {
                 </form>
                 <h1>{weatherData.city}</h1>
                 <ul>
-                    <li>{weatherData.date}</li>
+                    <li>
+                        <FormattedDate date={weatherData.date} />
+                    </li>
                     <li className="text-capitalize">{weatherData.description}</li>
                 </ul>
                 <div className="row mt-3">
