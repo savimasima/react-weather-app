4abfd652c35512a65aba36232372c609b6db0174
Savimasima <savimasima@gmail.com>
2022-11-13 15:02:03 +0000

    Added API integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity = "Kyiv"/>
         <footer>
           This project was coded by savimasima and is{" "}
           <a href="https://github.com/savimasima/react-weather-app">open-sourced on GitHub</a>
