ecd5349bc170c20109464de6231ebbf91105f7d4
Savimasima <savimasima@gmail.com>
2022-11-13 00:30:36 +0000

    Added Weather search

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
+import React from "react";
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
-
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by savimasima and is{" "}
-        <a 
-          href="https://github.com/savimasima/react-weather-app" 
-          target="_blank"
-        >open-sourced on GitHub</a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by savimasima and is{" "}
+          <a href="https://github.com/savimasima/react-weather-app">open-sourced on GitHub</a>
+        </footer>
+      </div>  
     </div>
   );
 }
