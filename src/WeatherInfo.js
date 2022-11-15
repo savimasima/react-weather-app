74b83ca5073239b159888ddf990bdd0fe0d3b190
Savimasima <savimasima@gmail.com>
2022-11-15 19:31:31 +0000

    Added icon Weather

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
     return (
@@ -13,15 +14,13 @@ export default function WeatherInfo(props) {
             </ul>
             <div className="row mt-3">
                 <div className="col-6">
-                    <div className="clearfix">
-                        <img 
-                            src={props.data.icon}
-                            alt={props.data.description}
-                            className="float-left"
-                        />     
-                        <div className="float-left">  
-                            <span className="temperature">{Math.round(props.data.temperature)}</span>
-                            <span className="unit">°C</span>
+                    <div className="row">
+                        <div className="col-3">
+                            <WeatherIcon code={props.data.icon} alt={props.data.description}/>
+                        </div>        
+                        <div className="col-4 p-4"> 
+                                <span className="temperature">{Math.round(props.data.temperature)}</span>
+                                <span className="unit">°C</span>
                         </div>     
                     </div>  
                 </div>
