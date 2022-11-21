41cdb8d8c286c84bcf7fd5605db2dc9b3f1d91d2
Savimasima <savimasima@gmail.com>
2022-11-21 16:47:36 +0000

    fixed adaptability

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -6,8 +6,8 @@ import WeatherTemperature from "./WeatherTemperature";
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      <h1>{props.data.city}</h1>
-      <ul>
+      <h1 className="description">{props.data.city}</h1>
+      <ul className="description">
         <li>
           <FormattedDate date={props.data.date} />
         </li>
@@ -16,16 +16,16 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-sm-6 col-12">
           <div className="row">
-            <div className="col-2 ps-3">
+            <div className="ps-3 col-2">
               <WeatherIcon code={props.data.icon} size={52} />
             </div>
 
-            <div className="col-4 ps-4">
+            <div className="ps-4 col-4">
               <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
         </div>
-        <div className="col-sm-6 col-12 pb-3">
+        <div className="col-sm-6 col-12 pb-3 humidity">
           <ul>
             <li>Humidity: {props.data.humidity}%</li>
             <li>Wind: {props.data.wind} km/h</li>
